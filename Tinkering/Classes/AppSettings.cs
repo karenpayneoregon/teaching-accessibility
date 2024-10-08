2af6732d02b9f3d2cd8834dc9af68dfecaf6d4a8
Karen Payne <paynekaren@comcast.net>
2024-10-08 06:53:42 -0700

    new project and AppSetting optimized.

--- Tinkering/Classes/AppSettings.cs
+++ Tinkering/Classes/AppSettings.cs
@@ -2,30 +2,14 @@
 using Tinkering.Classes.Configuration;
 using Tinkering.Models;
 
-
 namespace Tinkering.Classes;
 public class AppSettings
 {
-
-    public static Logging GetLogOptions()
-    {
-        Build();
-        return InitOptions<Logging>("LogOptions");
-    }
-
-    public static Role Role()
-    {
-        Build();
-        return InitOptions<Role>("Role");
-    }
-
-    public static FormSettings Settings()
-    {
-        Build();
-        return InitOptions<FormSettings>("FormSettings");
-    }
+    private static readonly IConfigurationRoot Configuration = Build();
+    public static Logging GetLogOptions() => InitOptions<Logging>("LogOptions");
+    public static Role Role() => InitOptions<Role>("Role");
+    public static FormSettings Settings() => InitOptions<FormSettings>("FormSettings");
     private static IConfigurationRoot Build() => ApplicationConfiguration.ConfigurationRoot();
-
     public static T InitOptions<T>(string section) where T : new()
-        => ApplicationConfiguration.ConfigurationRoot().GetSection(section).Get<T>();
+        => Configuration.GetSection(section).Get<T>();
 }
---
+++ UsingJavaScriptDebugger/readme.md
@@ -0,0 +1,8 @@
+﻿# About
+
+Code in this project came from reviewing web pages that were either not
+
+- Responsive
+- Accessible
+
+Most code under Lib folder can be better organized as the initial goal was to get pages responsive and accessible.
